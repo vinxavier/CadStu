026db5790080e8d81109419ac37d6724f1f8b75f
vinxavier <vsxavier@live.com>
2019-09-02 11:02:17 -0300

    limpeza de função não usada em actions.js

--- frontend/cadstu/src/store/actions.js
+++ frontend/cadstu/src/store/actions.js
@@ -1,16 +1,5 @@
-import Vue from "vue"
 export default {
   novoAluno: ({ commit }, payload) => {
     commit('adicionaAluno', payload)
-  },
-  getAddressFromCEP: ({ commit }, cep) => {
-    let url = 'https://viacep.com.br/ws/' + cep + '/json/'
-    Vue.axios.get(url)
-    .then((reponse) => {
-      return response
-    })
-    .catch((error =>{
-      console.log(error.statusText)
-    }))
   }
 }
